9555941f2de182bdc23e3e9a1819bc049d96e6f9
okmade <okmade@gmail.com>
2019-02-24 11:39:34 +0800

    Update 24-02-19

---
+++ Assets/StreamVideo.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+
+public class StreamVideo : MonoBehaviour {
+
+
+    public RawImage frame; 
+
+    private string sourceURL1 = "http://192.168.1.102:5000/video_feed";
+    private Texture2D texture;
+    private Stream stream;
+
+    public void Start()
+    {
+        texture = new Texture2D(2, 2);
+        // create HTTP request
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sourceURL1);
+        //Optional (if authorization is Digest)
+        //req.Credentials = new NetworkCredential("", "");
+        // get response
+        WebResponse resp = req.GetResponse();
+        // get response stream
+        stream = resp.GetResponseStream();
+        StartCoroutine(GetFrame());
+    }
+
+    IEnumerator GetFrame()
+    {
+        Byte[] JpegData = new Byte[200000];
+
+        while (true)
+        {
+            int bytesToRead = FindLength(stream);
+            //print(bytesToRead);
+            if (bytesToRead == -1)
+            {
+                print("End of stream");
+                yield break;
+            }
+
+            int leftToRead = bytesToRead;
+
+            while (leftToRead > 0)
+            {
+                leftToRead -= stream.Read(JpegData, bytesToRead - leftToRead, leftToRead);
+                yield return null;
+            }
+
+            MemoryStream ms = new MemoryStream(JpegData, 0, bytesToRead, false, true);
+
+            texture.LoadImage(ms.GetBuffer());
+            frame.texture = texture;
+            stream.ReadByte(); // CR after bytes
+            stream.ReadByte(); // LF after bytes
+        }
+    }
+
+    int FindLength(Stream stream)
+    {
+        int b;
+        string line = "";
+        int result = -1;
+        bool atEOL = false;
+        while ((b = stream.ReadByte()) != -1)
+        {
+            if (b == 10) continue; // ignore LF char
+            if (b == 13)
+            { // CR
+                if (atEOL)
+                {  // two blank lines means end of header
+                    stream.ReadByte(); // eat last LF
+                    return result;
+                }
+                if (line.StartsWith("Content-Length:"))
+                {
+                    result = Convert.ToInt32(line.Substring("Content-Length:".Length).Trim());
+                    //print(result);
+                }
+                else
+                {
+                    line = "";
+                }
+                atEOL = true;
+            }
+            else
+            {
+                atEOL = false;
+                line += (char)b;
+                //print(Convert.ToString(line));
+            }
+        }
+        return -1;
+    }
+}
