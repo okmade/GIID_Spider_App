4f3f31985300550c76bd02a60929301db24ef553
okmade <okmade@gmail.com>
2019-03-05 19:12:36 +0800

    Update 05_02_19

--- Assets/Control_Spider.cs
+++ Assets/Control_Spider.cs
@@ -1,22 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 
 public class Control_Spider : MonoBehaviour
 {
-    public Joystick joystickRight;
-    public Joystick joystickLeft;
-
-
+    public Joystick joystick_Right;
+    public Joystick joystick_Left;
+    private float waitTime = 5.0f;
+    private float timer = 0.0f;
+    private string sourceURL = "http://192.168.1.102:5000/mov";
+    
+    private string mode = "";
+    private string data_before = "";
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(PostData());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator PostData()
     {
+        while (true){
+            timer += Time.deltaTime;
+            if (timer > waitTime)
+            {
+                timer = timer - waitTime;
+
+                if (joystick_Right.Vertical* 10.0f >1)
+                {
+                    mode = "F";
+                }else if(joystick_Right.Vertical* 10.0f < -1)
+                {
+                    mode = "B";
+                }else
+                {
+                    mode = "S";
+                }
+                string data = "?mode=D&j1_v="+ mode + "&j1_h=20";
+
+                if (data != data_before){
+                    data_before = data;
+                    UnityWebRequest www = UnityWebRequest.Post(sourceURL+data,"");
+                    yield return www.SendWebRequest();
+                }else{
+                    yield return "No data";
+                }
         
+                /*if(www.isNetworkError || www.isHttpError) {
+                    Debug.Log(www.error);
+                }
+                else {
+                    Debug.Log("Form upload complete!");
+                }*/
+            }
+        }
     }
 }
